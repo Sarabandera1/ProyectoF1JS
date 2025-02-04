b5c6a204887ee8486647efdbd2e0c9bee1bc0ec4
Sarabandera1 <sarabanderaa@gmail.com>
2025-02-04 16:48:29 -0500

    feat: :sparkles: modelo3d

---
+++ simulacion/simulacion.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Ho3KHrFGsRy2Wy8F/scene.splinecode');
